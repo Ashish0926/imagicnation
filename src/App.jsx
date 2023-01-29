5c926e6be206a92b522a3122b4351a464ebf2d30
Ashish Gupta <ashishg0926@gmail.com>
2023-01-29 15:19:03 +0530

    updated home path

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,12 @@ const App = () => {
   return (
     <Router>
       <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
-        <Link to="/home">
+        <Link to="/">
           <img src={logo} alt="logo" className="w-28 object-contain" />
         </Link>
         <div className="flex gap-5">
           <Link
-            to="/home"
+            to="/"
             className="font-inter font-medium bg-black text-white px-4 py-2 rounded-md"
           >
             Home
@@ -28,7 +28,7 @@ const App = () => {
       
       <main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]">
         <Routes>
-          <Route exact path="/home" element={<Home />}></Route>
+          <Route exact path="/" element={<Home />}></Route>
           <Route exact path="/create-post" element={<CreatePost />}></Route>
         </Routes>
       </main>
